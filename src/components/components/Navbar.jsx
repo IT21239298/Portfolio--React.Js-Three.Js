6e8031e731bb61c0474ef46ce30e08cd7a7ce9f6
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-16 17:55:29 +0530

    nav bar

--- src/components/components/Navbar.jsx
+++ src/components/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${style.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${style.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -21,8 +21,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Sithum<span className="sm:block hiddn">| Js Mastery</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Sithum &nbsp;<span className="sm:block hiddn">| Js Mastery</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
