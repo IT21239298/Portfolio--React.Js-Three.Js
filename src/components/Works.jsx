8f207b61822ec2a7706a013a1163bd27e0466e91
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-20 01:04:53 +0530

    done works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -30,6 +30,34 @@ const ProjectCard = ({
             alt={name}
             className="w-full h-full object-cover rounded-2xl"
           />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10
+            rounded-full flex justify-center
+            items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2"
+                object-contain
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) =>(
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+
+            </p>
+          ))}
+
         </div>
       </Tilt>
     </motion.div>
