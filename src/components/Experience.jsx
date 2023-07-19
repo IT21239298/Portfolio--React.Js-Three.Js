fece845b69b8863c78ef46746acc2bf711c06c2f
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-19 12:28:04 +0530

    done experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -27,9 +27,26 @@ const ExperienceCard = ({ experience }) => {
         </div>
       }
     >
-      <div>
+      <div className="flex justify-center items-center w-full h-full">
         <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+        <p
+          className="text-secondary text-[16px] font-semibold"
+          style={{ margin: 0 }}
+        >
+          {experience.comapany_name}
+        </p>
       </div>
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px]
+           pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
     </VerticalTimelineElement>
   );
 };
