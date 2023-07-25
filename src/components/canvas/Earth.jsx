7689f0caff0849de98a253ccf7eb4c53b5160417
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-25 23:59:29 +0530

    Update Earth (find error)

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,10 +1,15 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
@@ -12,18 +17,28 @@ const EarthCanvas = () => {
     <Canvas
       shadows
       frameloop="demand"
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
-      <Suspense fallback={<CanvasLoader />} />
-      <OrbitControls
-        autoRotate
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-      />
-      <Earth />
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+
+        <Preload all />
+      </Suspense>
     </Canvas>
   );
 };
+
 export default EarthCanvas;
