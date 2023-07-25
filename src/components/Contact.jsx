3539556b874e7616fe337449d1fcd23800fc4acc
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-25 23:51:02 +0530

    import earth model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,21 @@ const Contact = () => {
             outlined-none border-none font-medium"
             />
           </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white
+          font-bold shadow-md shadow-primary rounded-xl"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
         </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0, 2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   );
