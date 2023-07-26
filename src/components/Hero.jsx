b3cb717e09f81e977e50eb3966797c4e7adfe8e4
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-26 23:34:38 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,8 +22,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#08C3EC]">Sithum</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
-            interfaces and web applications
+            Full Stack Developer...
+            <br className="sm:block hidden" />
           </p>
         </div>
       </div>
