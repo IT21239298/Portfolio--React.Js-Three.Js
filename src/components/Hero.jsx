3d3c8365246403bb6cf02f16a61b26b4d0d8cb5f
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-26 23:24:06 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,16 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-[#08C3EC]" />
+          <div
+            className="w-1 sm:h-70 h-40"
+            style={{ background: "linear-gradient(#08C3EC, #050816)" }}
+          />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Sithum</span>
+            Hi, I'm <span className="text-[#08C3EC]">Sithum</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />
