6f77747a936156083bb7a5a6eb3908be06e4ffa4
Sathsara Sithum <it21239298@my.sliit.lk>
2023-07-26 22:48:42 +0530

    logo size change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logo} alt="logo" className="w-13 h-12 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Sithum &nbsp;
             <span className="sm:block hidden"> | Sathsara</span>
